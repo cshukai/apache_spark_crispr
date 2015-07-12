41a22dd63806d82bc0f4f799abfab5c4fa9b16a7
sc724 <kz724@outlook.com>
2015-07-12 23:22:22 +0000

    seqprocessor readfile local

---
+++ SeqProcessor.java
@@ -0,0 +1,43 @@
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import java.io.Serializable;
+
+
+public class SeqProcessor implements Serializable{
+
+	public static void main(String [ ] args) throws Exception{
+		SparkConf conf=new SparkConf().setAppName("spark-crispr").setMaster("local[2]");
+		JavaSparkContext sc=new JavaSparkContext(conf); 		
+		SeqProcessor proc=new SeqProcessor();
+		JavaRDD<String> inputs=sc.textFile("bacteria/crispr/data/Xanthomonas_campestris_pv_campestris_str_atcc_33913.GCA_000007145.1.26.dna.chromosome.Chromosome.fa");
+		proc.subSeqFasta(inputs);
+	}
+
+	public void subSeqFasta(JavaRDD<String>  seqFiles){
+		
+		JavaRDD<Integer> lineLengths = seqFiles.map(new Function<String, Integer>() {
+			
+			public Integer call(String s) { return s.length(); }
+		});
+		int totalLength = lineLengths.reduce(new Function2<Integer, Integer, Integer>() {
+			
+			public Integer call(Integer a, Integer b) { return a + b; }
+		});
+		
+		System.out.println("total length is"+totalLength);
+
+	}
+
+
+}
+
+
+
+spark-submit \
+  --class "SeqProcessor" \
+  --master local[4] \
+  target/simple-project-1.0.jar
+
