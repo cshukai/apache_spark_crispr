ed9b79a553517bb043fa9a345a294f32e517b702
sc724 <kz724@outlook.com>
2015-07-15 18:54:10 +0000

    filter to remove line that contains AT-less for both left and right regions done

--- SeqProcessor.java
+++ SeqProcessor.java
@@ -7,6 +7,7 @@ import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import java.io.Serializable;
 import java.util.*;
+import scala.Tuple2;
 
 
 public class SeqProcessor implements Serializable{
@@ -15,9 +16,16 @@ public class SeqProcessor implements Serializable{
 		SparkConf conf=new SparkConf().setAppName("spark-crispr").setMaster("spark://masterb.nuc:7077");
 		JavaSparkContext sc=new JavaSparkContext(conf); 		
 		SeqProcessor proc=new SeqProcessor();
+		final double cutoff=0.70;
+
 		JavaRDD<String> inputs=sc.textFile("bacteria/crispr/data/Xanthomonas_campestris_pv_campestris_str_atcc_33913.GCA_000007145.1.26.dna.chromosome.Chromosome.fa");
-		JavaPairRDD<Double[],Long>  possibleLeadSeqs=proc.computeATfreq(inputs);
-		
+		JavaPairRDD<Double[],Long>  freq_region=proc.computeATfreq(inputs);
+		JavaPairRDD<Double[],Long>  possible_leader_region=proc.getATrichRegions(freq_region,cutoff);
+		JavaRDD<Double[]> test=possible_leader_region.keys();
+		JavaRDD<Long> rows=possible_leader_region.values();
+		Double[] test_arry=test.first();
+		System.out.println("left:"+test_arry[0]+"right:"+test_arry[1]);
+		System.out.println("first row "+rows.first());
 	}
 
 	public JavaPairRDD<Double[],Long> computeATfreq(JavaRDD<String>  seqFiles){
@@ -66,15 +74,25 @@ public class SeqProcessor implements Serializable{
 	}
 
 	//possibleLeadSeqs  String [left-freq, right-freq], lineNum
-//     public  JavaPairRDD findATrich(JavaPairRDD possibleLeadSeqs,int cutoff){
-//     	Function<Tuple2<Integer[], Long>, Boolean> AT_Filter =new Function<Tuple2<Integer[], Long>, Boolean>() {
-//     		public Boolean call(Tuple2<String, String> keyValue) {
-//     			String[] thisATfreq=keyValue._1();
+    public  JavaPairRDD<Double[],Long> getATrichRegions(JavaPairRDD<Double[],Long> freq_region ,double cutoff){
+    	final double cut=cutoff;
 
-//       			return (keyValue._1().length() < 20);
-//     		}
-//   		};
-//     }
+  		
+  		JavaPairRDD<Double[],Long> result=freq_region.filter(
+  				new Function<Tuple2<Double[], Long>, Boolean>() {
+    				@Override
+    				public Boolean call(Tuple2<Double[], Long> keyValue) {
+    					Double[] thisATfreq=keyValue._1();
+    					Long rowNum=keyValue._2();
+                        return (!(thisATfreq[0]<cut && thisATfreq[1]<cut) && !rowNum.equals("0") );
+    				}
+  				}
+
+  			);
+        return(result);
+    }
+
+
 
  }
 
