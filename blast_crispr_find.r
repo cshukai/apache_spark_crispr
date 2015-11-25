61f3ffbfae882ab89fa270700916d4f99eabb8e3
sc724 <kz724@outlook.com>
2015-11-25 22:04:55 +0000

    read fasta done

--- blast_crispr_find.r
+++ blast_crispr_find.r
@@ -1,4 +1,10 @@
-# on mri (lindberg) you have R image in /scratch and 
 library(Biostrings)
 library(ShortRead)
-pairwiseAlignment(pattern = , subject = "suuuuucdfasdfdsfsaeexxexxd",type="local")
+
+##processing and analysis of crispr db result
+crispr_db_repeat_path="/home/shchang/data/crisprdb/repeat_list.fa"
+db_repeat=readFasta(crispr_db_repeat_path)
+repeat_lens=width(sread(db_repeat))
+
+#pairwiseAlignment(pattern = , subject = "suuuuucdfasdfdsfsaeexxexxd",type="local")
+#http://stackoverflow.com/questions/9328519/how-to-read-from-multiple-fasta-files-with-r
