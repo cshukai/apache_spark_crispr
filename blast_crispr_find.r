c6adcb42b164cba2b093fc1db770e93fc49791c5
sc724 <kz724@outlook.com>
2015-11-25 23:25:07 +0000

    repeat boxplot done

--- blast_crispr_find.r
+++ blast_crispr_find.r
@@ -1,10 +1,16 @@
 library(Biostrings)
 library(ShortRead)
 
-##processing and analysis of crispr db result
-crispr_db_repeat_path="/home/shchang/data/crisprdb/repeat_list.fa"
+##processing and analysis of crispr db result##
+crispr_db_repeat_path="repeat_list.fa"
 db_repeat=readFasta(crispr_db_repeat_path)
-repeat_lens=width(sread(db_repeat))
 
+#analysis of repeat
+repeat_lens=width(sread(db_repeat))
+png("boxplot")
+ boxplot(repeat_lens)
+dev.off()
+#length distrubtion
+#
 #pairwiseAlignment(pattern = , subject = "suuuuucdfasdfdsfsaeexxexxd",type="local")
 #http://stackoverflow.com/questions/9328519/how-to-read-from-multiple-fasta-files-with-r
