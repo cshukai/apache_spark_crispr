b97915a9ed390379f498b4c3eb4c6e01ce9f948c
sc724 <kz724@outlook.com>
2015-11-26 20:39:48 +0000

    fasta id done

--- blast_crispr_find.r
+++ blast_crispr_find.r
@@ -1,10 +1,15 @@
 library(Biostrings)
 library(ShortRead)
+require(ggplot2)
 
 ##processing and analysis of crispr db result##
 crispr_db_repeat_path="repeat_list.fa"
 db_repeat=readFasta(crispr_db_repeat_path)
+db_repeat_ids=id(db_repeat)
 
+crispr_db_spacer_path="/home/shchang/data/crisprdb/spacer_list.fa"
+db_spacer=readFasta(crispr_db_spacer_path)
+db_spacer_ids=id(db_spacer)
 #analysis of repeat length
 repeat_lens=width(sread(db_repeat))
 png("boxplot")
@@ -42,22 +47,14 @@ colnames(arm_len)=colnames(ips_arm_len_cum)[2:ncol(ips_arm_len_cum)]
 
 for(i in 1:ncol(arm_len)){
   if(i!=ncol(arm_len)){
-   if(i!=ncol(arm_len)-1){
-      start_idx=i+1
-      end_idx=ncol(arm_len)
-      arm_len[,i]=arm_len[,i]-rowSums(arm_len[,start_idx:end_idx])
-   }
-   
-   else{
     arm_len[,i]=arm_len[,i]-arm_len[,i+1]
-   }
-   
   }
-  
-  
 }
 
+result_len=cbind(ips_arm_len_cum[,1],arm_len)
+colnames(result_len)=c("repeat_seq",as.character(min_arm_len:max_arm_len))
 
-#
+write.csv(result_len,file="result_len.csv",row.names=F)
+
 #pairwiseAlignment(pattern = , subject = "suuuuucdfasdfdsfsaeexxexxd",type="local")
 #http://stackoverflow.com/questions/9328519/how-to-read-from-multiple-fasta-files-with-r
