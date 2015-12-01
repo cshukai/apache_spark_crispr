b3d6c02e9a7692e5b302c10765d6eafe7b779e11
sc724 <kz724@outlook.com>
2015-12-01 16:31:39 +0000

    unzip done

--- preprocess_MRSMRS.r
+++ preprocess_MRSMRS.r
@@ -1,9 +1,20 @@
+library(snow)
+
 bac_collectoin_home="/home/sc724/data/seq/bacteria/ftp.ensemblgenomes.org/pub/bacteria/release-26/fasta"
 MRSMRS_perscript_home="/home/sc724/perlscript"
 
 #unzip raw data
+
 strain_path=paths=Sys.glob(file.path(bac_collectoin_home, "*", "*","dna"))
 gz_paths=Sys.glob(file.path(bac_collectoin_home, "*", "*","dna","*.gz"))
 genome_paths=Sys.glob(file.path(bac_collectoin_home, "*", "*","dna","*.dna.chromosome.Chromosome.fa.gz"))
+top_paths=Sys.glob(file.path(bac_collectoin_home, "*", "*","dna","*.toplevel.fa.gz"))
+
+for(i in 1:length(genome_paths)){
+cmd=paste("gunzip",genome_paths[i],sep=" ")
+system(cmd)
+}
+
+unzipped_genome_paths=Sys.glob(file.path(bac_collectoin_home, "*", "*","dna","*.dna.chromosome.Chromosome.fa"))
 
 save.image("crispr.RData")
