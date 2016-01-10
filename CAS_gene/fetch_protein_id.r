d7408bb01958c99fda31f88d690b61dea5355da9
sc724 <kz724@outlook.com>
2016-01-10 05:36:54 +0000

    gtf in done

--- CAS_gene/fetch_protein_id.r
+++ CAS_gene/fetch_protein_id.r
@@ -1,4 +1,4 @@
-
+library("ballgown")
 bac_collectoin_home="/home/shchang/data/ensemble/bac_r_29/ftp.ensemblgenomes.org/pub/release-29/bacteria/gtf"
 
 #unzip raw annotation data
@@ -12,4 +12,12 @@ system(cmd)
 unzip_gtf_path=gz_paths=Sys.glob(file.path(bac_collectoin_home, "*","*","*.gtf"))
 
 #extract target species 
-target_species=c()
+
+target_species=c("streptococcus_thermophilus_cnrz")
+
+protein_ids=list()
+for(i in 1:length(target_species)){
+   target_gtf=unzip_gtf_path[grep(pattern=target_species[i],x=unzip_gtf_path)]
+   gtf_tbl=gffRead(target_gtf)
+}
+save.image("gtf_protein.RData")
