d25bc81b84f0bb47811ceca66fd408ad5eb34513
sc724 <kz724@outlook.com>
2016-01-10 22:14:35 +0000

    get rid of quotes for protein id

--- CAS_gene/fetch_protein_id.r
+++ CAS_gene/fetch_protein_id.r
@@ -14,10 +14,23 @@ unzip_gtf_path=gz_paths=Sys.glob(file.path(bac_collectoin_home, "*","*","*.gtf")
 #extract target species 
 
 target_species=c("streptococcus_thermophilus_cnrz")
-
-protein_ids=list()
+targetIdx=NULL
 for(i in 1:length(target_species)){
-   target_gtf=unzip_gtf_path[grep(pattern=target_species[i],x=unzip_gtf_path)]
-   gtf_tbl=gffRead(target_gtf)
+   foundIdx=grep(pattern=target_species[i],x=unzip_gtf_path)
+   if(length(foundIdx)==1){
+      targetIdx=c(targetIdx,foundIdx)
+   }
 }
+
+protein_id_list=list()
+for(i in 1:length(targetIdx)){
+   target_gtf=unzip_gtf_path[targetIdx[i]]
+   gtf_tbl=gffRead(target_gtf)
+   gtf_tbl_CDS=gtf_tbl[which(gtf_tbl[,"feature"]=="CDS"),]
+   target_protein_id=getAttributeField(gtf_tbl_CDS$attributes,field = "protein_id")
+   target_protein_id=gsub(pattern="\"",replacement="",x=target_protein_id)
+   protein_id_list[[i]]=target_protein_id
+}
+
+names(protein_id_list)=target_species
 save.image("gtf_protein.RData")
