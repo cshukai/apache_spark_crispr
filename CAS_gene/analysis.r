49043dab09a40d058a30183def81f6e773a37033
sc724 <kz724@outlook.com>
2016-01-17 03:49:50 +0000

    test of thrid party code to fetch conding region done

--- CAS_gene/analysis.r
+++ CAS_gene/analysis.r
@@ -1,6 +1,6 @@
+####################match by sequence similarity###########
 output_home="/home/shchang/scratch/cas_blast/streptococcus_thermophilus"
 result_paths=Sys.glob(file.path(output_home, "*.out.txt"))
-
 cas_type=NULL
 for(i in 1:length(result_paths)){
    tmp=unlist(strsplit(split="/",x=result_paths[i]))
@@ -17,12 +17,26 @@ for(i in  1: length(cas_type)){
   identity_max=max(this_result[,3])
   minimum_e_value=min(this_result[,11])
   max_bit_score=max(this_result[,12])
-  result_statistics=rbind(c(cas_type[i],identity_max,max_bit_score,minimum_e_value))
+  result_statistics=rbind(result_statistics,c(cas_type[i],identity_max,max_bit_score,minimum_e_value))
   top_match_list[[i]]=this_result[intersect(which(this_result[,3] >seq_identity_cuff),which(this_result[,11] <e_cuff)),2] # grab the highly similar reference region
 }
 
+names(top_match_list)=cas_type
 
-
-
+write.csv(result_statistics,file="result.statistics.csv",row.names=F)
 names(top_match_list)=cas_type
 save.image("analysis.RData")
+################match by location #############
+top_match_list_transformed=top_match_list
+match_ref_region=NULL
+for(i in 1:length(top_match_list)){
+   this_group_result=top_match_list[[i]]
+   if(length(this_group_result)>0){
+     this_cas_type=names(top_match_list)[i]
+     for(j in 1:length(this_group_result)){
+         temp=as.character(this_group_result[j])
+         temp2=unlist(strsplit(split="|",x=temp))
+          
+     }    
+   }
+}
---
+++ CAS_gene/protienId2gene.pl
@@ -0,0 +1,103 @@
+#/usr/bin/perl
+
+#use strict;
+
+use warnings;
+
+use Bio::DB::GenBank;
+
+use Bio::SeqIO;
+
+use Bio::SeqFeatureI;
+
+
+
+#open my $OUTFILE, '>', "Gb_parser.fasta";
+
+my $gb = new Bio::DB::GenBank; 
+
+# Récupération dans $seq de l'objet Genbank contenant de nombreuses informations
+
+# Gi, Acc, séquence, Annotations, Organisme, Espèce, Genre ...
+
+my $proteinID=$ARGV[0]
+
+my @Acc =$proteinID; #Accession number de la séquence à traiter
+
+my $seq1 = $gb->get_Seq_by_acc(@Acc);
+
+my $Sequence = $seq1->seq();
+
+my $Description = $seq1->desc();
+
+print "Acc = @Acc\nDescription = $Description\n"; #ecrit le num d'accession et sa description associée
+
+my $seqio_object = $seq1;
+
+
+
+for my $feat_object ($seqio_object->get_SeqFeatures) {
+
+    #print "\n", $feat_object->primary_tag, "\n";
+
+    for my $tag ($feat_object->get_all_tags){
+
+        print $tag." ";
+
+        for my $value ($feat_object->get_tag_values($tag)){
+
+            print  $value."\n"      
+
+        }
+
+    }
+
+}
+
+
+
+foreach $feat ( $seqio_object->get_SeqFeatures() ) {
+
+    if ($feat->primary_tag() eq "CDS"){ # ne s'intéresse qu'aux tags "CDS"
+
+	foreach $tag ( $feat->get_all_tags() ) {    
+
+	    if ($tag eq "locus_tag"){
+
+		print  "\n>the locus: ", join(' ',$feat->get_tag_values($tag)); #ecrit le locus
+
+	    }
+
+	    elsif ($tag eq "product"){
+
+		print  " ", join(' ',$feat->get_tag_values($tag)); #ecrit le nom du CDS
+
+	    }
+
+	    elsif ($tag eq "coded_by"){
+
+		print "\n\nThe corresponding gene acc: ", $feat->get_tag_values($tag);
+
+            #print ">\n\n", join (' ', $feat-> get_tag_values($tag));     #affiche l'acc. du gene
+
+
+
+	    }
+
+	}
+
+
+
+     #print "\n"
+
+	print " \n\n", $feat->start, "..", $feat->end,"\n\n";;# " on strand ",     $feat->strand, "\n";
+
+	$out = $feat->seq; #out est un Primary::Seq
+
+	$string = $out->seq(); #recupère la séquence
+
+	print $string."\n\n";
+
+    }
+
+}
