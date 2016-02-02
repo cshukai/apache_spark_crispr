4a1b309e46e9aea9ddd66fe2660833a179f2eea7
sc724 <kz724@outlook.com>
2016-02-02 22:38:27 +0000

    blast automation prepear to remove code to generate idv directory

--- CAS_gene/blast_autmoation.r
+++ CAS_gene/blast_autmoation.r
@@ -3,8 +3,27 @@ all_species=dir()
 
 for(i in  1:length(all_species)){
     tmp=paste('/share/sw/blast/2.2.30+/bin/makeblastdb -in',all_species[i],sep=" ")
-    tmp2=paste(tmp,'-dbtype prot -parse_seqids',sep=" ")
+    tmp2=paste(tmp,'-dbtype prot -parse_seqids -out ',sep=" ")
     tmp3=paste(all_species[i],"db",sep=".")
     cmd=paste(tmp2,tmp3,sep=" ")
     system(cmd)
 }
+
+dir.create("data")
+# generate folders
+for(i in  1:length(all_species)){
+    allFiles=Sys.glob(file.path(paste(all_species[i],"*",sep="")))
+    dest=paste("data/",all_species[i],sep="")
+    dir.create(dest)
+    for(j in 1:length(allFiles)){
+        file.copy(from=allFiles[j],to=dest)
+    }
+
+}
+
+save.image("crispr_without_analysis.RData")
+
+##fetch corresponding cas proteins
+library(Biostrings)
+ref_aa=readAAStringSet("crispr_protein.fasta",format="fasta")
+ref_id=names(ref_aa)
