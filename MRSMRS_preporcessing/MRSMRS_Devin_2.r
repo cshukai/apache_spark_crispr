7c88064b55c40c9cc105ce9a5e6796af6400e447
sc724 <kz724@outlook.com>
2016-03-30 21:48:25 +0000

    generate run.sh fto start from palindrome as basic unit

--- MRSMRS_preporcessing/MRSMRS_Devin_2.r
+++ MRSMRS_preporcessing/MRSMRS_Devin_2.r
@@ -39,15 +39,12 @@ for(i in 1:length(unzipped_top_refined_paths)){
 
 
 #upload data to hdfs
-cleanedDatasets=Sys.glob(file.path("..","intermediate_data","*.clean"))
+cleanedDatasets=Sys.glob(file.path("/home/sc724/intermediate_data","*.clean"))
 for(i in 1:length(cleanedDatasets)){
    tmp=paste("hadoop fs -put",cleanedDatasets[i],sep=" ")
-   cmd=paste(tmp,"/sc724",sep=" ")
+   cmd=paste(tmp,"intermediate_data",sep=" ")
    cat(cmd,file="upload.sh",append=T,fill=T)
 }
-
-save.image("preprocessing.RData")
-
 
 #generate run script
 hdfs_filenames=NULL
@@ -60,27 +57,29 @@ hdfs_filenames=c(hdfs_filenames,this_name)
 prefix='spark-submit  --class "PalindromeFinder" --master yarn-client --driver-memory 6G  --executor-memory 6G  --num-executors 3 target/scala-2.10/palindromefinder_2.10-0.1.jar'
 #prefix='spark-submit  --class "PalindromeFinder" --driver-memory 6G  --executor-memory 6G  --num-executors 3 target/scala-2.10/palindromefinder_2.10-0.1.jar'
 min_repeat_len=15
-min_stemLoop_size=11
-max_stemLoop_size=16
 min_palin_arm=4
 
 
-for(j in min_stemLoop_size:max_stemLoop_size){
-    kmer_len=j
+    kmer_len=min_palin_arm
     argu=NULL
+    argu_2=NULL
     for(i in 1:length(hdfs_filenames)){
         #tmp=paste("/",hdfs_filenames[i],sep="")  #
         tmp=hdfs_filenames[i]
         this_argu=paste(tmp,kmer_len,sep="  ")
+        second_argu=paste(tmp,min_repeat_len,sep="  ")
         argu=c(argu,this_argu)
+        argu_2=c(argu_2,second_argu)
     }
 
     for(i in 1:length(argu)){
         cmd=paste(prefix,argu[i],sep=" ")
+        cmd_2=paste(prefix,argu_2[i],sep=" ")
         cat(cmd,file="run.sh",append=T,fill=T)
+        cat(cmd_2,file="run.sh",append=T,fill=T)
 
     }
 
-}
-#todo : add a block to compute additional k mer when palindrome size doesn't include min repeat unit size for tracrRNA
+
+
 save.image("preprocessing.RData")
