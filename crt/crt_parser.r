d35b07d33fd97277e0e4a3446081753a2142f806
sc724 <kz724@outlook.com>
2016-04-05 21:00:29 +0000

    crt verfied to has no truncated case

--- crt/crt_parser.r
+++ crt/crt_parser.r
@@ -2,6 +2,8 @@ library("Biostrings")
 #assuming current directory is the folder containing all the crt output file
 out_files=Sys.glob(file.path("*"))
 
+
+#parse crt output into tabular format
 result=NULL
 for(i in 1:length(out_files)){
    thisStrain=out_files[i]
@@ -32,7 +34,7 @@ for(i in 1:length(out_files)){
              }
              
                 thisPalinNum=length(findComplementedPalindromes(DNAString(thisRepeatSeq),min.armlength=4,max.looplength=8))
-                thisRow=c(thisStrain,thisCRISPRarrID,thisRepeatStartLoc,thisRepeatSeq,thisSpacerSeq)
+                thisRow=c(thisStrain,thisCRISPRarrID,thisRepeatStartLoc,thisRepeatSeq,thisSpacerSeq,thisPalinNum)
                 result=rbind(result,thisRow)
         
          }
@@ -48,6 +50,39 @@ for(i in 1:length(out_files)){
 
 
 }
-
+colnames(result)=c("strain","crispr_num","repeat_start","rep_unit","spacer","palin_num")
 write.csv(result,"../crt_summary.csv",row.names=F)
 save.image("../crt_parser.RData")
+
+
+
+# update your R workspace location
+#analysis of truncated cases
+truncated_result=NULL
+strains_with_crispr=names(table(result[,1]))
+for(i in 1: length(strains_with_crispr)){
+   this_subset=result[which(result[,"strain"] %in% strains_with_crispr[i]),]
+   arr_id=names(table(this_subset[,"crispr_num"]))
+   for(j in 1:length(arr_id)){
+       this_crispr_arr=this_subset[which(this_subset[,"crispr_num"] %in% arr_id[j]),]
+       all_repeat_unit=nchar(this_crispr_arr[,"rep_unit"])
+       this_range=range(all_repeat_unit)
+       max_rep_unit_len=this_range[2]
+       min_rep_unit_len=this_range[1]
+       if(max_rep_unit_len!=min_rep_unit_len){
+          for(k in 1:nrow(this_crispr_arr)){
+              this_rep_unit_len=nchar(this_crispr_arr[k,"rep_unit"])
+              if(this_rep_unit_len!=max_rep_unit_len) {
+                 thisRow=c(strains_with_crispr[i],arr_id[j],k,nrow(this_crispr_arr),this_rep_unit_len,max_rep_unit_len)
+                 truncated_result=rbind(truncated_result,thisRow)                  
+              }
+          }
+
+       }
+   }
+}
+
+
+save.image("crt_parser.RData")
+
+
