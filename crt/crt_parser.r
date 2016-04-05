80172612fd3d9d70468046af9b3c6d8fda92ab98
sc724 <kz724@outlook.com>
2016-04-05 16:31:00 +0000

    large scale crt summary successfully output

--- crt/crt_parser.r
+++ crt/crt_parser.r
@@ -10,9 +10,13 @@ for(i in 1:length(out_files)){
       chr_len=unlist(strsplit(split=": ",x=d[2]))[2]
       cr_record_star_idx=grep(pattern="POSITION",d)+2
       cr_record_end_idx=grep(pattern="Repeats: ",d)-2
-      
-      for(j in 1:length(cr_record_start_idx)){
+      print(thisStrain)
+      print(cr_record_star_idx)
+      print(cr_record_end_idx)
+      for(j in 1:length(cr_record_star_idx)){
          thisCRISPRarrID=j
+         
+         
          for(k in cr_record_star_idx[j]:cr_record_end_idx[j]){
              thisLine=d[k]
              tmp=unlist(strsplit(x=thisLine,split="\t"))
@@ -30,10 +34,7 @@ for(i in 1:length(out_files)){
                 thisPalinNum=length(findComplementedPalindromes(DNAString(thisRepeatSeq),min.armlength=4,max.looplength=8))
                 thisRow=c(thisStrain,thisCRISPRarrID,thisRepeatStartLoc,thisRepeatSeq,thisSpacerSeq)
                 result=rbind(result,thisRow)
-             
-             
-             
-             
+        
          }
       }
       
@@ -47,4 +48,6 @@ for(i in 1:length(out_files)){
 
 
 }
-save.image("crt_parser.RData")
+
+write.csv(result,"../crt_summary.csv",row.names=F)
+save.image("../crt_parser.RData")
