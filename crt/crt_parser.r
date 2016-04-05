2ef69b0b4a44a7a58367bf3c0e25ea3bac9b46f5
sc724 <kz724@outlook.com>
2016-04-05 03:17:54 +0000

    idv test done , large scale test to do

--- crt/crt_parser.r
+++ crt/crt_parser.r
@@ -4,20 +4,35 @@ out_files=Sys.glob(file.path("*"))
 
 result=NULL
 for(i in 1:length(out_files)){
- 
-   d=readLines(file(out_files[i]))
+   thisStrain=out_files[i]
+   d=readLines(file(thisStrain))
    if(length(grep(pattern="CRISPR elements were found",x=d))==0){
       chr_len=unlist(strsplit(split=": ",x=d[2]))[2]
       cr_record_star_idx=grep(pattern="POSITION",d)+2
       cr_record_end_idx=grep(pattern="Repeats: ",d)-2
       
       for(j in 1:length(cr_record_start_idx)){
+         thisCRISPRarrID=j
          for(k in cr_record_star_idx[j]:cr_record_end_idx[j]){
              thisLine=d[k]
              tmp=unlist(strsplit(x=thisLine,split="\t"))
              thisRepeatStartLoc=tmp[1]
              thisRepeatSeq=tmp[3]
-             thisSpacerSeq=tmp[4]
+             if(k!=cr_record_end_idx[j]){
+            
+               thisSpacerSeq=tmp[4]
+             } 
+          
+             else{
+               thisSpacerSeq="End"
+             }
+             
+                thisPalinNum=length(findComplementedPalindromes(DNAString(thisRepeatSeq),min.armlength=4,max.looplength=8))
+                thisRow=c(thisStrain,thisCRISPRarrID,thisRepeatStartLoc,thisRepeatSeq,thisSpacerSeq)
+                result=rbind(result,thisRow)
+             
+             
+             
              
          }
       }
