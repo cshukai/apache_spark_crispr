6ab9bb813156a219272e82098524b06c7465569b
sc724 <kz724@outlook.com>
2016-07-10 00:02:26 +0000

    second filtering run

--- Devin/compare2Validate.r
+++ Devin/compare2Validate.r
@@ -97,12 +97,14 @@ save.image("validation.RData")
 library(ShortRead)
 library(Biostrings)
 reserverdList=NULL
-for(i in 1:length(mrsmrs_species)){
-    this_file=fasta[which(fasta ==mrsmrs_species[i])]
+for(i in 2726:length(mrsmrs_species)){
+    this_file=fasta[grep(pattern=mrsmrs_species[i],fasta,ignore.case=T)]
+    print(i)
+    print("+++++++")
     if(length(this_file)==1){
-        d=readFasta(this_file)
+        d=readFasta(this_file[1])
         x=toString(unlist(sread(d)))
-        this_d=mrsmrs[which(mrsmrs[,1] == mrsmrs_species[i]),]
+        this_d=mrsmrs[which(mrsmrs[,1] %in% mrsmrs_species[i]),]
         all_array_id=unique(this_d[,2])
         for(j in 1:length(all_array_id)){
             this_array_d=this_d[which(this_d[,2] %in% all_array_id[j] ),]
@@ -114,9 +116,15 @@ for(i in 1:length(mrsmrs_species)){
                 all_seq=c(all_seq,this_seq)
             }
             
-            if(sum(diff(nchar(all_seq)))==0){
+            if(sum(diff(nchar(all_seq)))==0 &&length(which(is.na(all_seq)))==0){
                  for(k in 1:nchar(all_seq[1])){
                      estimator=nucleotideFrequencyAt(DNAStringSet(all_seq),at=1,as.prob=T)
+                     if(max(estimator)<0.75){
+                         break
+                     }
+                 }
+                 if(k==nchar(all_seq[1])){
+                     reserverdList=rbind(reserverdList,this_array_d)
                  }
             }
        
@@ -124,9 +132,43 @@ for(i in 1:length(mrsmrs_species)){
     
     }
     else{
-        print("warn")
+        print(this_file)
+        print(i)
+        print("-----")
+    
     }
 }
+
+refineList=NULL
+for(i in 1:nrow(reserverdList)){
+            print(i)
+            this_d=reserverdList[which(reserverdList[,1] %in% mrsmrs_species[i]),]
+            all_array_id=unique(this_d[,2])
+            for(j in 1:length(all_array_id)){
+            this_array_d=this_d[which(this_d[,2] %in% all_array_id[j] ),]
+            this_start=as.numeric(this_array_d[1,3])
+            this_end=this_array_d[nrow(this_array_d),3]+this_array_d[nrow(this_array_d),4]-1
+            this_arr_size=this_end-this_start+1
+            if(this_arr_size>=75 && length(this_arr_size)>0){
+                refineList=rbind(refineList,this_array_d)
+            }
+
+            }
+        }
+
+#arch analysis
+crt_rep_seq=as.character(crt[,4])
+piler_rep_seq=as.numeric(piler[,4])
+summary(nchar(crt_rep_seq))
+summary(piler_rep_seq)
+
+#novel structure
+d=readFasta("/home/shchang/data/bac_29_fasta/ftp.ensemblgenomes.org/pub/release-29/bacteria/fasta/bacteria_20_collection/acetobacter_pasteurianus_ifo_3283_03/dna/Acetobacter_pasteurianus_ifo_3283_03.GCA_000010845.1.29.dna.chromosome.Chromosome.fa")
+x=toString(unlist(sread(d)))
+substr(x,2212367,2212421)
+substr(x,2212376,2212376+55-1)
+substr(x,2212385,2212385+55-1)
+
 #venn diagram
 library(gplots)
 venn( list(Spark=mrsmrs_species,CRT=crt_species,PilerCR=piler_species) )
